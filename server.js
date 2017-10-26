36e840eebc7555ce4eafdce0dac1b32387fb5d3a
jailbreak26 <jailbreak31@gmail.com>
2017-10-26 13:40:52 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,13 @@
-// Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '0.0.0.0';
-// Listen on a specific port via the PORT environment variable
-var port = process.env.PORT || 8080;
-
-// Grab the blacklist from the command-line so that we can update the blacklist without deploying
-// again. EDGE Connector is open by design, and this blacklist is not used, except for countering
-// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
-// use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.EDGECONNECTOR_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.EDGECONNECTOR_WHITELIST);
-function parseEnvList(env) {
-  if (!env) {
-    return [];
-  }
-  return env.split(',');
-}
-
-// Set up rate-limiting to avoid abuse of the public EDGE Connector server.
-var checkRateLimit = require('./lib/rate-limit')(process.env.EDGECONNECTOR_RATELIMIT);
+#!/usr/bin/env node
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
+var port = process.env.PORT || 1337;
 
 var cors_proxy = require('./lib/edge-connector');
 cors_proxy.createServer({
-  originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
-  requireHeader: ['origin', 'x-requested-with'],
-  checkRateLimit: checkRateLimit,
-  removeHeaders: [
-    'cookie',
-    'cookie2',
-    // Strip Heroku-specific headers
-    'x-heroku-queue-wait-time',
-    'x-heroku-queue-depth',
-    'x-heroku-dynos-in-use',
-    'x-request-start',
-  ],
-  redirectSameOrigin: true,
-  httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
-    xfwd: false,
-  },
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with', 'content-type', 'accept'],
+    requireHeader: [],
+    removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
-  console.log('Running EDGE Connector on ' + host + ':' + port);
+    console.log('Running EDGE Connector on ' + host + ':' + port);
 });
