3a1936bc008b1f6fbf36ee30bd5d9e7b66351cc4
jailbreak26 <jailbreak31@gmail.com>
2021-07-23 16:12:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 var port = process.env.PORT || 1337;
 
-var cors_proxy = require('./lib/edge-connector');
+var cors_proxy = require('./lib/pixel-connector');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with', 'content-type', 'accept'],
     requireHeader: [],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
-    console.log('Running EDGE Connector on ' + host + ':' + port);
+    console.log('Running Pixel Connector on ' + host + ':' + port);
 });
