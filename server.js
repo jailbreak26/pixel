dc7bee712c5b3260cb1a7004eb1a07608e2171fc
jailbreak26 <jailbreak31@gmail.com>
2023-09-23 00:55:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-var port = process.env.PORT || 8880;
+var port = process.env.PORT || 1337;
 
 var cors_proxy = require('./lib/pixel-connector');
 cors_proxy.createServer({
