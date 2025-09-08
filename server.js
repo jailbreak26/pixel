123e4444e24ee9a9d427f5c53230bb85c63bf432
jailbreak26 <jailbreak31@gmail.com>
2025-09-08 20:06:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var cors_proxy = require('./lib/pixel-connector');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with', 'content-type', 'accept'],
-    requireHeader: [],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
     console.log('Running Pixel Connector on ' + host + ':' + port);
